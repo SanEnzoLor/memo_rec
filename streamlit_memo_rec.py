c852ce730fe0fc819471146ba5ea4db2d242b51a
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-01-07 10:19:04 +0100

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -205,6 +205,10 @@ def main():
     st.markdown("https://pubmed.ncbi.nlm.nih.gov/15081887/")
     st.write(f"Durata registrazione {record_seconds} secondi")
 
+    # Placeholder per il testo
+    if "text_visible" not in st.session_state:
+        st.session_state.text_visible = False  # Indica se mostrare il campo di testo
+    
     # Bottone per avviare la registrazione
     if st.button("Inizia registrazione"):
         st.warning("Attere il salvataggio dei dati prima di selezionare nuovamente **Inizia registrazione**.")
@@ -215,9 +219,6 @@ def main():
             st.session_state.used_words = []  # Parole già utilizzate
         if "remaining_words" not in st.session_state:
             st.session_state.remaining_words = cue_words.copy()  # Parole rimanenti
-        # Placeholder per il testo
-        if "text_visible" not in st.session_state:
-            st.session_state.text_visible = False  # Indica se mostrare il campo di testo
         if "user_text" not in st.session_state:
             st.session_state.user_text = ""  # Testo inserito dall'utente
 
