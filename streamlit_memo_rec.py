d1b5f8536045454ba6d09d690655a8fbccf00eac
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-01-07 16:10:12 +0100

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -257,8 +257,8 @@ def main():
             st.session_state.used_words = []  # Parole già utilizzate
         if "remaining_words" not in st.session_state:
             st.session_state.remaining_words = cue_words.copy()  # Parole rimanenti
-        if "user_text" not in st.session_state:
-            st.session_state.user_text = ""    # Testo utente
+
+        user_text = ""    # Testo utente
 
         # Se non ci sono parole da suggerire, disabilita il pulsante di registrazione
         if len(st.session_state.remaining_words) == 0:
@@ -280,7 +280,7 @@ def main():
 
         # Mostra il campo di testo se abilitato
         if text_visible == True:
-            st.session_state.user_text = st.text_input("Scrivi qui il tuo testo:")
+            user_text = st.text_input("Scrivi qui il tuo testo:")
                 
         # Loop per il timer
         while time.time() - start_time < record_seconds:
@@ -307,7 +307,7 @@ def main():
             "PCL-5-hyperarousal": results_p[3],
             "PCL-5-tot": results_p[4],
             "Cue-Word": selected_word,
-            "Testo": st.session_state.user_text
+            "Testo": user_text
         })
 
         # Rimuovi la parola utilizzata dalla lista
