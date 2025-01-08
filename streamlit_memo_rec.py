d901b3061eb583babb81c57602d42858b29257f0
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-01-08 12:13:14 +0100

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -256,7 +256,7 @@ def main():
         st.session_state.remaining_words = cue_words.copy()  # Parole rimanenti
     if "user_text" not in st.session_state:
         st.session_state.user_text = ""    # User text
-    text = ""
+    text = []
 
     
     # Bottone per avviare la registrazione
@@ -291,7 +291,7 @@ def main():
             timer_placeholder.markdown(f"**Tempo rimanente: {remaining_time} secondi**")
             time.sleep(1)  # Aspetta un secondo
 
-        text = st.session_state.user_text
+        text.append(st.session_state.user_text)
         #text_visible = False    # Nasconde la casella di testo
         
         # Scaduto il tempo
@@ -316,7 +316,7 @@ def main():
             "PCL-5-hyperarousal": results_p[3],
             "PCL-5-tot": results_p[4],
             "Cue-Word": selected_word,
-            "Testo": text
+            "Testo": text[10-len(st.session_state.remaining_words)]
         })
             
         st.write(f"Il testo scritto è: {st.session_state.user_text}")
