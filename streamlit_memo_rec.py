5bd775f51f8906522e40661c92ff708b224ebf9f
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-01-13 15:49:39 +0100

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -265,7 +265,7 @@ def main():
     if st.session_state.session_data:
         file = data_csv(st.session_state.session_data)
         if st.download_button(label = "Salva Dati", data = file, file_name = "dati.csv"):
-            st.warning("Grazie per aver partecipato al task. Ora per completare il salvataggio **invia una mail** a **lorenzocarozzi9826@gmail.com** avente in **allegato** il file appena scaricato **dati.csv**.")
+            st.warning("Grazie per aver partecipato al task. Ora per completare il salvataggio **invia una mail** a **lorenzocarozzi9826@gmail.com** avente in **allegato** il file appena scaricato '**dati.csv**'.")
             # Svuota lo stato della sessione
             st.session_state.clear() 
             st.cache_resource.clear()
