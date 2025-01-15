9344ef4a67d6496970395cc6fc758e72852558dd
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-01-15 17:38:45 +0100

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -235,7 +235,7 @@ def main():
 
     visible = lambda x: "collapsed" if x else "visible"
     testo = st.text_area("Scrivi qui il tuo testo una volta cliccato su **Inizia** e aver visto la **parola** da cui recuperare la memoria:", height = 300, key = len(st.session_state.remaining_words), disabled = not show, label_visibility = visible(show))
-    button_v = lambda x0, x1: len(x0) == 0 and not x1
+    button_v = lambda x0, x1: not x1
     
     if len(st.session_state.remaining_words) != 0:
         if st.button("Salva memoria", disabled = button_v(testo, show)):
@@ -263,7 +263,6 @@ def main():
             st.session_state.remaining_words.remove(st.session_state.selected_word)
             st.success(f"Registrazione completata. Dati salvati temporaneamente.")
             show = False
-            testo = ""
             
 
     # Bottone per salvare i dati
