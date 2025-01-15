7b257860e3744a93253ba024651660740b880bc1
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-01-15 19:08:22 +0100

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -200,6 +200,7 @@ def main():
     cue_words = ['ECCITATO', 'ANNOIATO', 'FELICE', 'FALLITO', 'FORTUNATO', 'DISPERATO', 'RILASSATO', 'SOLITARIO', 'SERENO', 'TRISTE']
     if gender == "Femminile":
         cue_words = cue_words_f
+    st.write(cue_words, gender, gender == "Femminile")
     st.write("Il task consiste nel **ricordare e scrivere** un **evento personale** richiamato dalla **parola** che verrà mostrata una volta selezionato **Inizia**. Nel testo si descrivano quanti più **dettagli** possibili associati alla memoria autobiografica recuperarta. L'evento raccontato **NON** deve essere accaduto durante la **scorsa settimana**.")
     st.write("Terminata la scrittura sarà possibile salvare la memoria appena descritta (selezionando **Salva memoria**), il task potrà essere rieseguito per un massimo di 10 volte con parole differenti (selezionando nuovamente **Inizia** e poi **Salva memoria**). Se si desidera ci si può fermare prima (selezionando **Salva Dati**).")
     st.markdown("https://doi.org/10.1080/09658211.2018.1507042")
