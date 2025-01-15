f387b53851642936205a849b8673a5cade74f8bc
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-01-15 15:40:00 +0100

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -235,9 +235,10 @@ def main():
 
     visible = lambda x: "collapsed" if x else "visible"
     testo = st.text_area("Scrivi qui il tuo testo una volta cliccato su **Inizia** e aver visto la **parola** da cui recuperare la memoria:", height = 300, key = len(st.session_state.remaining_words), disabled = not show, label_visibility = visible(show))
-
+    button = lambda x: True if len(x) == 0 else False
+    
     if len(st.session_state.remaining_words) != 0:
-        if st.button("Salva memoria", disabled = if len(testo) != 0):
+        if st.button("Salva memoria", disabled = button(testo)):
             duration = time.time() - st.session_state.start_time
             # Aggiungi i dati di questa registrazione alla sessione
             st.session_state.session_data.append({
