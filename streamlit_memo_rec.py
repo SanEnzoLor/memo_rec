718453f18258144686183d41b146533314dcd38f
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-01-15 16:02:31 +0100

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -238,7 +238,7 @@ def main():
     testo = st.text_area("Scrivi qui il tuo testo una volta cliccato su **Inizia** e aver visto la **parola** da cui recuperare la memoria:", height = 300, key = len(st.session_state.remaining_words), disabled = not show, label_visibility = visible(show))
     
     if len(st.session_state.remaining_words) != 0:
-        if st.button("Salva memoria", disabled = button(testo)):
+        if st.button("Salva memoria", disabled = button(testo), key = 0):
             duration = time.time() - st.session_state.start_time
             # Aggiungi i dati di questa registrazione alla sessione
             st.session_state.session_data.append({
@@ -259,31 +259,37 @@ def main():
                 "Time": duration
             })
 
-        elif st.button("Salva memoria", disabled = not show):
-            duration = time.time() - st.session_state.start_time
-            # Aggiungi i dati di questa registrazione alla sessione
-            st.session_state.session_data.append({
-                "Eta": eta,
-                "Gender": gender,
-                "Nazionalita": nazione,
-                "Educazione": educazione,
-                "Occupazione": occupazione,
-                "BDI2": results_d,
-                "RRS" : results_r,
-                "PCL-5-reexperiencing": results_p[0], 
-                "PCL-5-avoidance": results_p[1],
-                "PCL-5-altereted_cognition": results_p[2],
-                "PCL-5-hyperarousal": results_p[3],
-                "PCL-5-tot": results_p[4],
-                "Cue-Word": st.session_state.selected_word,
-                "Text": testo,
-                "Time": duration
-            })
-                
             # Rimuovi la parola utilizzata dalla lista
             st.session_state.remaining_words.remove(st.session_state.selected_word)
             st.success(f"Registrazione completata. Dati salvati temporaneamente.")
             show = False
+
+        if show:
+            if st.button("Salva memoria", key = 1):
+                duration = time.time() - st.session_state.start_time
+                # Aggiungi i dati di questa registrazione alla sessione
+                st.session_state.session_data.append({
+                    "Eta": eta,
+                    "Gender": gender,
+                    "Nazionalita": nazione,
+                    "Educazione": educazione,
+                    "Occupazione": occupazione,
+                    "BDI2": results_d,
+                    "RRS" : results_r,
+                    "PCL-5-reexperiencing": results_p[0], 
+                    "PCL-5-avoidance": results_p[1],
+                    "PCL-5-altereted_cognition": results_p[2],
+                    "PCL-5-hyperarousal": results_p[3],
+                    "PCL-5-tot": results_p[4],
+                    "Cue-Word": st.session_state.selected_word,
+                    "Text": testo,
+                    "Time": duration
+                })
+                
+                # Rimuovi la parola utilizzata dalla lista
+                st.session_state.remaining_words.remove(st.session_state.selected_word)
+                st.success(f"Registrazione completata. Dati salvati temporaneamente.")
+                show = False
 
     # Bottone per salvare i dati
     if st.session_state.session_data:
