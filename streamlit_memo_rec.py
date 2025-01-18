54722b42bb30f6243bc221bdb766fc66bd27545f
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-01-18 12:23:09 +0100

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -416,7 +416,7 @@ def main():
     
     wav_audio_data = st_audiorec()
     if wav_audio_data is not None:
-        transcribe_audio(st.audio(wav_audio_data, format='audio/wav'))
+        transcribe_audio(wav_audio_data)
 
 
 
